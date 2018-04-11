c8779c36864621cb2a69762916c0c370d462b72c
Allen <809721414@qq.com>
2018-04-11 20:55:14 +0800

    【update】修改超时时间为10s

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import router from './router';
 Vue.config.productionTip = false;
 Vue.prototype.axios = axios.create({
   baseURL: 'http://localhost:8888',
-  timeout: 1000,
+  timeout: 10000,
   headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
   transformRequest: [data => Qs.stringify(data)],
 });
